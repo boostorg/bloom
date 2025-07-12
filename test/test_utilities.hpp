574de8375a8e13d742a70f610a7f90cf7e52de7d
joaquintides <joaquin@tid.es>
2025-07-12 09:35:09 +0200

    fixed typedef

--- test/test_utilities.hpp
+++ test/test_utilities.hpp
@@ -120,7 +120,7 @@ class input_iterator
 public:
   using iterator_category=std::input_iterator_tag;
   using value_type=typename traits::value_type;
-  using difference_type=typename traits::value_type;
+  using difference_type=typename traits::difference_type;
   using pointer=typename traits::pointer;
   using reference=typename traits::reference;
 
